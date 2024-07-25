712f25bb36d23fd5f52838ea2327ff48301732df
DVTecno <verasdiegoca78@gmail.com>
2024-07-25 11:03:13 -0300

    Add base setup for video API

---
+++ videollamada/src/main/java/com/videollamda/VideoLaamadaApplication.java
@@ -0,0 +1,13 @@
+package com.videollamda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoLaamadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoLaamadaApplication.class, args);
+	}
+
+}
---
+++ videollamada/src/test/java/com/videollamda/VideoLaamadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videollamda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoLaamadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
